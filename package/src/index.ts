07adee3b06dacdc92fb0bc81969829934981dcf7
frelindo <frelindo@tumo.world>
2022-10-05 19:08:32 +0200

    create fallback 404 response

--- package/src/index.ts
+++ package/src/index.ts
@@ -16,6 +16,10 @@ app.get('/search/:query', (req, res) => {
     res.redirect(`https://google.com/search?q=${query}`)
 })
 
+app.get('/*', (req, res) => {
+    res.send('<h1>404 Not Found</h1>\nOno I didn\'t find this page :<')
+})
+
 app.listen(3000, () => {
     console.log(`The server has been started on port 3000!`)
 })

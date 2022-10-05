e71aecb0416aa237196b05513bbcf7ae1afcd965
frelindo <frelindo@tumo.world>
2022-10-05 18:58:31 +0200

    create response for name/

--- package/src/index.ts
+++ package/src/index.ts
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello, World!</h1>')
 })
 
+app.get('/name/:name', (req, res) => {
+    const name = req.params.name.replace(/\b\w/g, c => c.toUpperCase())
+    res.send(`<h1>Hello, ${name}!</h1>`)
+})
+
 app.listen(3000, () => {
     console.log(`The server has been started on port 3000!`)
 })

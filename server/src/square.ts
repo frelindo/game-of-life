7886d4c3ac01022b0b4cea3c9fa9b61cdcde4de1
frelindo <frelindo@tumo.world>
2022-10-08 13:10:20 +0200

    remove square.ts

--- server/src/square.ts
+++
@@ -1,7 +0,0 @@
-import os from 'os'
-import Square from './structures/Square'
-
-console.log(`The operating system is ${os.platform}`)
-
-const square = new Square(10)
-console.log(`Area: ${square.area}`)

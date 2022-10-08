11afbbfaca301e6463c057bf8da409a08567b218
frelindo <frelindo@tumo.world>
2022-10-08 12:35:09 +0200

    ignore lib

--- dom/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const p = document.getElementById('pElement');
-console.log(p);
-let clicks = 0;
-p === null || p === void 0 ? void 0 : p.addEventListener('click', (event) => {
-    console.log(`Click #${++clicks}`);
-});
